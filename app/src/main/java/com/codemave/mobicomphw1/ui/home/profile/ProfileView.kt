9029cb6fda523d7ac722c7c1176056e235bc7f88
aemrehu <aemrehu@gmail.com>
2023-03-17 07:30:43 +0200

    camera stuff added, not working

--- app/src/main/java/com/codemave/mobicomphw1/ui/home/profile/ProfileView.kt
+++ app/src/main/java/com/codemave/mobicomphw1/ui/home/profile/ProfileView.kt
@@ -1,6 +1,8 @@
 package com.codemave.mobicomphw1.ui.home.profile
 
 import android.content.Context
+import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -8,6 +10,8 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
@@ -18,6 +22,8 @@ import androidx.navigation.NavController
 import com.codemave.mobicomphw1.R
 import com.codemave.mobicomphw1.SharedPreferences
 import com.codemave.mobicomphw1.ui.login.Logged
+import java.io.File
+import java.util.concurrent.ExecutorService
 
 @Composable
 fun ProfileScreen(
@@ -85,3 +91,26 @@ private fun TopBar(
         backgroundColor = backgroundColor
     )
 }
+
+// Camera stuff
+
+//private lateinit var outputDirectory: File
+//private lateinit var cameraExecutor: ExecutorService
+//
+//private var shouldShowCamera: MutableState<Boolean> = mutableStateOf(false)
+//
+//private fun handleImageCapture(uri: Uri) {
+//    Log.i("kilo", "Image captured: $uri")
+//    shouldShowCamera.value = false
+//}
+//
+//private fun getOutputDirectory(): File {
+//    val mediaDir = externalMediaDirs.firstOrNull()?.let {
+//        File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+//    }
+//
+//    return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
+//}
+//
+//outputDirectory = getOutputDirectory()
+//cameraExecutor = Executors.newSingleThreadExecutor()
